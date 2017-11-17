4b6b23baf74a0d44640e8056e7bf10d47eafa382
Caitlin Trussell <caitlintrussell@gmail.com>
2017-11-17 01:27:16 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -1,9 +1,20 @@
 function IcebreakerResponse(email){
   this.email = email
 }
-function BatchCreate(data) {
+IceBreakerResponse.BatchCreate(data) {
   return map.data(function(userEmail){
     return new IcebreakerResponse(userEmail)
   })
-  
+
 }
+// 
+// function User(name, email){
+// this.name = name
+// this.email = email
+// }
+//  
+// User.BatchCreate = function(data){
+// return data.map(function(userData){
+// return new User(userData[0], userData[1])
+// })
+// }
